3b97a6360ab6bb32933b6a20e777530de1f77059
Giorgos Georgiou <93381413+lokowp@users.noreply.github.com>
2023-01-07 16:17:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# making our own functions
+def my_function():
+  print("Hello")
+  print("Bye")
+
+def my_function_2():
+  # Do this
+  # Then do this
+  # Finally do this
+
+  
+# I have to use my fucntion in order to be executed. *CALLING FUNCTIONS*
+my_function()
